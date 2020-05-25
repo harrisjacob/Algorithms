bf1f0d782f7f543379e5c8b722920ab119954d4c
Jacob Harris <harrisjakef@gmail.com>
2020-05-24 19:57:33 -0700

    Brent's algorithm

--- Cycle-Detection/findCycle.cpp
+++ Cycle-Detection/findCycle.cpp
@@ -1,9 +1,13 @@
+#include <algorithm>
 #include "helperFunctions.h"
+
+
 void usage(char* prog){
 	std::cout << "Usage:" << std::endl;
 	std::cout << "\t-h\t help"<<std::endl;
-	std::cout << "\t-f\t specify data stream [mandatory]"<<std::endl;
-	std::cout << "\t-l\t add loop to data [optional]"<<std::endl;
+	std::cout << "\t-f [file]\t specify data stream [mandatory]"<<std::endl;
+	std::cout << "\t-l[loop index]\t add loop to data [optional] (note: no space)"<<std::endl;
+	std::cout << "\t-a [algorithm]\t specify algorithm (Floyd/Brent) [optional]"<<std::endl;
 }
 
 void floyd(Node<int>* head){
@@ -31,7 +35,39 @@ void floyd(Node<int>* head){
 		loopLength++;
 	}
 
-	std::cout << "Loop found starting at element index " << startLoop <<" with loop length "<<loopLength<<std::endl;
+	std::cout << "Floyd's algorithm found a loop starting at element index " << startLoop <<" with loop length "<<loopLength<<"."<<std::endl;
+
+}
+
+void brent(Node<int>* head){
+	int power = 0, loopLength = 0, startLoop = 0;
+	Node<int> *tortise, *hare;
+
+	tortise = head;
+	hare = head->getNext();
+
+	while(tortise->getData() != hare->getData()){
+		if(power == loopLength){
+			tortise = hare;
+			power *=2;
+			loopLength = 0;
+		}
+		hare = hare->getNext();
+		loopLength++;
+	}
+
+	tortise = hare = head;
+	for(int i = 0; i < loopLength; i++){
+		hare = hare->getNext();
+	}
+
+	while(tortise->getData() != hare->getData()){
+		tortise = tortise->getNext();
+		hare = hare->getNext();
+		startLoop++;
+	}
+
+	std::cout << "Brent's algorithm found a loop starting at element index " << startLoop <<" with loop length "<<loopLength<<"."<<std::endl;
 
 }
 
@@ -45,10 +81,10 @@ int main(int argc, char** argv){
 
 	int opt;
 	int loopDest = 0;
-	std::string userFile;
+	std::string userFile, algorithm = "floyd";
 	bool addLoop = false;
 	//if help flag
-	while((opt = getopt(argc, argv, "hf:l::")) != -1 ){
+	while((opt = getopt(argc, argv, "hf:l::a:")) != -1 ){
 		switch(opt){
 			case 'h':
 				usage(argv[1]);
@@ -62,6 +98,9 @@ int main(int argc, char** argv){
 					loopDest = std::stoi(optarg);
 				}
 				break;
+			case 'a':
+				algorithm = optarg;
+				break;
 			default:
 				usage(argv[1]);
 				return EXIT_FAILURE;
@@ -69,10 +108,22 @@ int main(int argc, char** argv){
 	}
 
 	Node<int>* head = readIn(userFile);
-	//printNodes(head);
-	//if(addLoop) addTailLoop(head, loopDest);
-	floyd(head);
 	
+	std::for_each(algorithm.begin(), algorithm.end(), [](char &c){
+		c = tolower(c);
+	});
+
+	//printNodes(head);
+	if(addLoop) addTailLoop(head, loopDest);
+	if(algorithm.compare("floyd") == 0){
+		floyd(head);
+	}else if(algorithm.compare("brent") == 0){
+		brent(head);
+	}else{
+		std::cout<<"Error: Unknown algorithm specified"<<std::endl;
+		usage(argv[1]);
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
