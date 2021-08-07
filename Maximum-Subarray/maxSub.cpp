c65eef4ce33347b22734f5fe8480eb9213d0ea63
Jacob Harris <harrisjakef@gmail.com>
2021-08-07 15:13:12 -0700

    Included mergesort library in makefile

--- Maximum-Subarray/maxSub.cpp
+++ Maximum-Subarray/maxSub.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "mergeSort.h"
+
+void printArray(int a[], int len){
+	for(int i=1; i<len; i++){
+		std::cout << a[i-1] << ", ";
+	}
+	std::cout << a[len-1] << std::endl;
+}
+
 
 int main(int argc, char** argv){
 
+	int numArray[] = {5, 8, 3, 5, 2, 7, 6, 1};
+	int len = sizeof(numArray)/sizeof(numArray[0]);
+
+
+	std::cout << "Original array: ";
+	printArray(numArray, len);
+
+	mergeSort(numArray, 0, len-1);
+
+
+	std::cout << "Sorted array: ";
+	printArray(numArray, len);
+	
 	return 0;
 }
