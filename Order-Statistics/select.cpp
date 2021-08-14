79688dc850b68b4941f4cd3c32e7ee40dc15fdbb
Jacob Harris <harrisjakef@gmail.com>
2021-08-14 15:47:12 -0700

    Intial select algorithm

--- Order-Statistics/select.cpp
+++ Order-Statistics/select.cpp
@@ -27,22 +27,45 @@ int partition(int arr[], int start, int end, int part){
 	return i;
 }
 
-void insertionSort(int vals[], int len){
-
+void insertionSort(int vals[], int start, int len){
+	
 	int key, pos;
-	for(int i=1; i<len; i++){
+	for(int i=1+start; i<start+len; i++){
 		key = vals[i];
 		pos = i-1;
-		while (pos>0 && vals[pos]>=key){
+		while (pos>start-1 & vals[pos]>=key){
 			vals[pos+1] = vals[pos];
 			pos--;
 		}
 		vals[pos+1] = key;
 	}
-
 }
 
-int select(int vals[], int rank){
+int select(int vals[], int len, int rank){
+
+	int groupSize = 5;
+
+	int groups = len / groupSize;
+	int remaining = len % groupSize;
+
+	//Is there a partial group that is unfilled?
+	groups += (!remaining) ? 0 : 1;
+
+
+	//insertion sort the members of each of the groups
+	//ignore the final group
+	for(int groupNum=0; groupNum<groups-1; groupNum++){
+		insertionSort(vals, groupNum*groupSize, groupSize);
+	}
+
+	//insertion sort the members of the last group
+	if(!remaining){
+		insertionSort(vals, (groups-1)*groupSize, groupSize); //Group is completely filled
+	}else{
+		insertionSort(vals, (groups-1)*groupSize, remaining); //Group is partially filled
+	}
+
+
 
 	return 0;
 }
@@ -58,18 +81,18 @@ void printArray(int arr[], int len){
 
 int main(int argc, char** argv){
 
-	int vals[] = {1, 33, 7, 3, 51, 2, 6, 10, 13, 64, 4, 103, 9, 24};
+	int vals[] = {1, 33, 7, 3, 51, 10, 6, 13, 2, 64, 103, 9, 4, 24};
 	int len = sizeof(vals)/sizeof(vals[0]);
 	int rank = 0;
 
 	printArray(vals, len);
 
-	insertionSort(vals, len);
+	select(vals, len, 0);
 	//std::cout << pos << std::endl;
 
 	printArray(vals, len);
 
-	//std::cout << "The element with rank " << rank << " is " << select(vals, rank) << std::endl;
+	//std::cout << "The element with rank " << rank << " is " << select(vals, len, rank) << std::endl;
 
 	return 0;
 }
